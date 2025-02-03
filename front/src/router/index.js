7bb22506258674aaf5d3822f0c8acf81faa38f4a
Yehor Prykhodko <yehor.prykhodko8@etu.univ-lorraine.fr>
2025-02-03 12:28:58 +0200

    front base

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,22 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/views/Home.vue'
+import Game from '@/views/Game.vue'
+import Connexion from '@/views/Connexion.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+    { path: '/', name: 'Home', component: Home },
+    { path: '/Connexion', name: 'Connexion', component: Connexion },
+    { path: '/game', name: 'Game', component: Game }
   ],
 })
 
---
+++ front/src/views/Connexion.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="connexion">
+      <div class="switch">
+        <button @click="vueActive = 'Login'">Se connecter</button>
+        <button @click="vueActive = 'Signup'">S'inscrire</button>
+      </div>
+      <component :is="vueActive"></component>
+    </div>
+  </template>
+  
+  <script>
+  import Login from '@/components/Connexion/Login.vue';
+  import Signup from '@/components/Connexion/Signup.vue';
+  export default {
+    name: 'Connexion',
+    components: { Login, Signup },
+    data() {
+      return { vueActive: 'Login' }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .switch {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  .switch button {
+    margin: 0 10px;
+    padding: 8px 16px;
+  }
+  </style>
+  
