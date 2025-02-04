6390e9ad73178e583c0528e4f1f770b12bf9ab1f
Gaetan Pinot <gaetan.pinot6@etu.univ-lorraine.fr>
2025-02-04 17:46:12 +0100

    auth env

---
+++ gateway/config/routes.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+use Geoquizz\Auth\application\actions\GetUtilisateur;
+use Geoquizz\Auth\application\actions\GetUtilisateurById;
+use Geoquizz\Auth\application\actions\PostSignup;
+use Geoquizz\Auth\application\actions\ValidateTokenAction;
+use Geoquizz\Auth\application\actions\PostSignIn;
+use Geoquizz\Auth\application\renderer\JsonRenderer;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Routing\RouteCollectorProxy;
+
+return function (\Slim\App $app): \Slim\App {
+
+
+    $app->get("[/]", function ($request, $response) {
+
+        return JsonRenderer::render($response, 200, ["message" => "Api geoquizz auth"]);
+    });
+
+    $app->post('/login[/]', PostSignIn::class)->setName('signIn');
+
+    $app->post('/signup[/]', PostSignup::class)->setName('signup');
+
+    $app->get('/validateToken[/]', ValidateTokenAction::class)->setName('ValidateToken');
+
+    $app->get('/utilisateur/{id}', GetUtilisateurById::class)->setName('getUtilisateur');
+
+    $app->get('/utilisateur[/]', GetUtilisateur::class)->setName('getUtilisateurs');
+
+    $app->options('/{routes:.+}', function ($request, $response, $args) {
+        return $response;
+    });
+
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+        throw new HttpNotFoundException($request);
+    });
+    return $app;
+
+};
