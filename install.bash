bc015a2cde51bb2dc336d151932299efa1f828e9
Gaetan Pinot <gaetan.pinot6@etu.univ-lorraine.fr>
2025-02-07 14:15:30 +0100

    install avec build du front

--- install.bash
+++ install.bash
@@ -4,11 +4,10 @@ docker compose run --rm mailer composer install
 docker compose run --rm game composer install
 docker compose run --rm auth composer install
 docker compose run --rm gateway composer install
+docker compose run --rm front npm install
+docker compose run --rm front npm run build
 docker compose up -d
-# docker compose exec game composer install
-# docker compose exec auth composer install
-# docker compose exec gateway composer install
-docker compose exec game php bin/console doctrine:migrations:migrate
-docker compose exec game php bin/console doctrine:fixture:load
-docker compose exec auth php bin/console doctrine:migrations:migrate
-docker compose exec auth php bin/console doctrine:fixture:load
+# docker compose exec game php bin/console doctrine:migrations:migrate
+# docker compose exec game php bin/console doctrine:fixture:load
+# docker compose exec auth php bin/console doctrine:migrations:migrate
+# docker compose exec auth php bin/console doctrine:fixture:load
